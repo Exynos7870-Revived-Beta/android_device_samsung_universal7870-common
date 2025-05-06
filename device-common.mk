0dcd8f406e5c1ff7be3f711e67475f89947d903c
Ksawlii <ksawery.blaszczak@proton.me>
2025-05-06 19:40:09 +0000

    universal7870-common: Bump camera provider to 2.5
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -126,8 +126,8 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0-helper \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5-impl \
+    android.hardware.camera.provider@2.5-service \
     libion_exynos \
     libstagefright_shim \
     camera.exynos7870 \
